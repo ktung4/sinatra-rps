9d021df8a9510394f06bf2c7b5412f31af32214d
ktung4 <ktung@chicagobooth.edu>
2024-10-20 14:11:29 +0000

    sinatra_rps

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "better_errors"
+require "binding_of_caller"
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/rock") do
+  moves = ["rock","paper","scissors"]
+  @comp_move = moves.sample
+  if @comp_move == "rock"
+    @outcome ="tied"
+  elsif @comp_move=="paper"
+    @outcome="lost"
+  else
+    @outcome="won"
+  end
+  
+  erb(:zebra)
 end
